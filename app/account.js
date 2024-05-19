dcba10685f9db5fc0adb0f6860a608d169102e4f
michellelim04 <18221052@std.stei.itb.ac.id>
2024-05-19 18:01:10 +0700

    fix: account, landing

--- app/account.js
+++ app/account.js
@@ -1,37 +1,36 @@
-import { View, Text } from "react-native"
-import { Icon } from "react-native-paper"
-import { SafeAreaView } from "react-native-safe-area-context"
-import Button from "./components/button"
-import { useAuth } from "../auth/AuthProvider"
+import { View, Text } from "react-native";
+import { Icon } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Button from "./components/button.js";
+import { useAuth } from "../auth/AuthProvider";
 
 const Account = ({ navigation }) => {
-  const { signOut, user } = useAuth()
+  const { signOut, user } = useAuth();
 
   const logout = async () => {
-    await signOut()
-    navigation.navigate("Landing")
-  }
+    await signOut();
+    navigation.navigate("Landing");
+  };
 
   return (
     <SafeAreaView>
       <View className="flex-1 min-h-screen justify-center items-center bg-white">
         <View className="w-full items-center">
           <View className="my-2">
-            <Icon source="account-circle" size={100} color="#EEC759"/>
+            <Icon source="account-circle" size={100} color="#EEC759" />
           </View>
           <View>
-            <Text className="font-MontserratSemiBold text-lg text-[#EEC759] text-center">{user.email}</Text>
+            <Text className="font-MontserratSemiBold text-lg text-[#EEC759] text-center">
+              {user.email}
+            </Text>
           </View>
           <View className="items-center my-3">
-            <Button
-              label="Sign Out"
-              onPress={() => logout()}
-            />
+            <Button label="Sign Out" onPress={() => logout()} />
           </View>
         </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
--- app/landing.js
+++ app/landing.js
@@ -5,31 +5,45 @@ import { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Landing({ navigation }) {
-  const { isLoggedIn } = useAuth()
+  const { isLoggedIn } = useAuth();
 
   useEffect(() => {
-    if(isLoggedIn) {
-      navigation.navigate("Home")
+    if (isLoggedIn) {
+      navigation.navigate("Home");
     }
-  }, [isLoggedIn])
+  }, [isLoggedIn]);
 
   return (
     <SafeAreaView>
       <View className="flex w-full justify-center items-center h-screen">
         <View className="w-full items-center">
-          <Image source={require("./assets/images/5301126.png")} className="w-full"/>
+          <Image
+            source={require("../assets/images/5301126.png")}
+            className="w-full"
+          />
         </View>
         <View className="bg-[#FFFBE9] flex-1 w-full px-4 py-5">
           <View className="items-center">
-            <Text className="text-[#9BB8CD] text-3xl font-MontserratExtraBold">Monitor Your dorm,</Text>
-            <Text className="text-[#9BB8CD] text-3xl font-MontserratExtraBold">anytime,</Text>
-            <Text className="text-[#9BB8CD] text-3xl font-MontserratExtraBold">anywhere !</Text>
+            <Text className="text-[#9BB8CD] text-3xl font-MontserratExtraBold">
+              Monitor Your dorm,
+            </Text>
+            <Text className="text-[#9BB8CD] text-3xl font-MontserratExtraBold">
+              anytime,
+            </Text>
+            <Text className="text-[#9BB8CD] text-3xl font-MontserratExtraBold">
+              anywhere !
+            </Text>
           </View>
           <View className="px-5 items-center pb-3 pt-5">
-            <Text className="text-center font-MontserratSemiBold text-[#F5DC95]">We helpyou monitor your  dorm, just throught your phone</Text>
+            <Text className="text-center font-MontserratSemiBold text-[#F5DC95]">
+              We helpyou monitor your dorm, just throught your phone
+            </Text>
           </View>
           <View className="my-3 items-center">
-            <TouchableOpacity onPress={() => navigation.navigate("SignIn")} className="bg-[#B1C381] h-[40px] w-[100px] rounded-md px-5 items-center justify-center">
+            <TouchableOpacity
+              onPress={() => navigation.navigate("SignIn")}
+              className="bg-[#B1C381] h-[40px] w-[100px] rounded-md px-5 items-center justify-center"
+            >
               <View>
                 <Text className="text-white font-MontserratBold">Sign In</Text>
               </View>
